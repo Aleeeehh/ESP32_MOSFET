4eed17d2d21d8a58339218f27c77c8ba3825dfa5
Aleeeehh <alessioprato03@gmail.com>
2025-06-28 14:09:18 +0200

    Inital setup for esp32

--- src/esp32_main.cpp
+++ src/esp32_main.cpp
@@ -1,15 +1,21 @@
 #include <Arduino.h>
 
-namespace esp32 {
-const int ledPin = 35; // LED interno esp32
-// const int ledPin = 4; // Lucetta integrata ESP32-CAM per flash
+namespace esp32
+{
+  const int ledPin = 35; // LED alternativo su alcune schede ESP32
+  // const int ledPin = 4; // Lucetta integrata ESP32-CAM per flash
 
-void blinkLed() {
-  digitalWrite(ledPin, LOW);  // Accendi il LED (logica invertita)
-  delay(200);                 // Aspetta 2000 ms
-  digitalWrite(ledPin, HIGH); // Spegni il LED (logica invertita)
-  delay(200);                 // Aspetta 2000 ms
-}
+  void blinkLed()
+  {
+    digitalWrite(ledPin, HIGH); // Accendi il LED
+    delay(2000);                // Aspetta 2000 ms
+    digitalWrite(ledPin, LOW);  // Spegni il LED
+    delay(2000);                // Aspetta 2000 ms
+  }
 
-void setupLed() { pinMode(ledPin, OUTPUT); }
+  void setupLed()
+  {
+    pinMode(ledPin, OUTPUT);
+    digitalWrite(ledPin, LOW); // Inizia spento
+  }
 } // namespace esp32
--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,13 @@
-#include "esp32cam_main.h"
-#include "webserver_main.h"
+#include "esp32_main.h"
 #include <Arduino.h>
 
-void setup() {
-  // esp32cam::setupLed();
+void setup()
+{
+  esp32::setupLed();
   Serial.begin(115200);
-
-  webserver::setupCamera();
-  webserver::setupWiFi();
-  webserver::setupServer();
 }
 
-void loop() {
-  // esp32cam::blinkLed();
-  webserver::loop();
+void loop()
+{
+  esp32::blinkLed();
 }
